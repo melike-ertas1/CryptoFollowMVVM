6056de4c4bed52c3b787ea14acf983d4d521777d
Melike Ertaş <melikeertas1@gmail.com>
2021-12-13 11:08:51 +0300

    Initial Commit

---
+++ CryptoFollow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoFollow
+//
+//  Created by melike ertaş on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
